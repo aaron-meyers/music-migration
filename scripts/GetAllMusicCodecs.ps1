d3e93ee3436f31ecc334d89f8949c4454fa201dd
Aaron Meyers <13340718+aaron-meyers@users.noreply.github.com>
2023-06-30 11:54:44 -0700

    GetAllMusicCodecs.ps1

---
+++ scripts/GetAllMusicCodecs.ps1
@@ -0,0 +1,30 @@
+param(
+    [Parameter()]
+    [String]
+    $Path = 'codecs.csv',
+
+    [Parameter()]
+    [Switch]
+    $Force
+)
+
+if (Test-Path $Path) {
+    # Backup existing codecs.csv first
+    if ($Force -and (Test-Path "$Path.bak")) {
+        Remove-Item "$Path.bak"
+    }
+    Move-Item $Path "$Path.bak"
+}
+
+$extensions = @(
+    '*.wma',
+    '*.m4a',
+    '*.flac',
+    '*.mp3',
+    '*.ogg'
+)
+
+$output = foreach ($ext in $extensions) {
+    GetMusicCodec -Filter $ext -First -Recurse
+}
+$output | Export-Csv $Path
